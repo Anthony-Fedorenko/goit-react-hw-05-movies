27bbbb5857b3317db1706f899381577048bf2573
Anthony <anthony.fedorenko@gmail.com>
2021-09-10 15:20:45 +0300

    all done!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import { lazy, Suspense } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Loader from 'react-loader-spinner';
+import AppBar from './components/AppBar/AppBar';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const HomeView = lazy(() => import('./views/HomeView/HomeView' /* webpackChunkName: "HomeView"*/),);
+const MoviesView = lazy(() => import('./views/MoviesView/MoviesView' /* webpackChunkName: "MoviesView"*/),);
+const MovieView = lazy(() => import('./views/MovieView/MovieView' /* webpackChunkName: "MovieView"*/),);
+
+
+export default function App() {
+    return (
+        <>
+            <AppBar />
+
+            <Suspense fallback={<Loader type="ThreeDots" color="#00BFFF" height={80} width={80} />}>
+                <Switch>
+                    <Route path="/" exact>
+                        <HomeView />
+                    </Route>
+
+                    <Route path="/movies" exact>
+                        <MoviesView />
+                    </Route>
+
+                    <Route path="/movies/:movieId">
+                        <MovieView />
+                    </Route>
+
+                    {/*<Route>*/}
+                    {/*    <NotFoundView />*/}
+                    {/*</Route>*/}
+
+                </Switch>
+            </Suspense>
+        </>
+    );
+}
